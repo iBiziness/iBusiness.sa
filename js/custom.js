a9941411e78b0f425a96b99ed025fc88bd5b9946
Abubaker Shamlan <abubaker@shamlan.com.sa>
2016-02-13 19:35:04 +0300

    adding cd-methodology-img

--- js/custom.js
+++ js/custom.js
@@ -234,13 +234,13 @@ $(function () {
 
         function hideBlocks(blocks, offset) {
             blocks.each(function(){
-                ( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+                ( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-methodology-img, .cd-timeline-content').addClass('is-hidden');
             });
         }
 
         function showBlocks(blocks, offset) {
             blocks.each(function(){
-                ( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+                ( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img, .cd-methodology-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
             });
         }
     });
