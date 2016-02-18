05a4e7256a2b28f6d4b82ef7b9aa78ce5cc00b87
Ruel Angelo Aballe <ruel.aballe@aestrea.com>
2016-02-19 02:32:57 +0800

    Update hover and active.

--- js/custom.js
+++ js/custom.js
@@ -246,19 +246,11 @@ $(function () {
 
         // timeline change color
         $('.cd-timeline-img').on('click', function (e) {
-            if($($(this).data('target')).hasClass('in')){
-                var defaultColor = $(this).data('default-color');
-                if(defaultColor)
-                    $(e.currentTarget).css('background-color', defaultColor);
-                else
-                    $(e.currentTarget).css('background-color', '#E8E8E8');
+            if($($(this).data('target')).length && !$(this).hasClass('active')){
+                $(this).addClass('active');
             }
-            else if($($(this).data('target')).length){
-                var dataCollapseColor = $(this).data('collapse-color');
-                if(dataCollapseColor)
-                    $(e.currentTarget).css('background-color', dataCollapseColor);
-                else
-                    $(e.currentTarget).css('background-color', 'red');
+            else{
+                $(this).removeClass('active')
             }
         });
 
